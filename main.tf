03c451f74762b52db8248cc5da963be501c1e4dd
Joshua Crunden <joshuacrunden@gmail.com>
2021-11-08 22:45:41 +0000

    Add SES verification

--- main.tf
+++ main.tf
@@ -457,3 +457,20 @@ resource "aws_iam_role" "contact" {
 }
 POLICY
 }
+
+resource "aws_ses_domain_identity" "domain" {
+  domain = var.domain_name
+}
+
+resource "aws_ses_domain_dkim" "domain" {
+  domain = aws_ses_domain_identity.domain.domain
+}
+
+resource "aws_route53_record" "amazonses_dkim_record" {
+  count   = 3
+  zone_id = data.aws_route53_zone.philomusica_hosted_zone.zone_id
+  name    = "${element(aws_ses_domain_dkim.domain.dkim_tokens, count.index)}._domainkey"
+  type    = "CNAME"
+  ttl     = "600"
+  records = ["${element(aws_ses_domain_dkim.domain.dkim_tokens, count.index)}.dkim.amazonses.com"]
+}
