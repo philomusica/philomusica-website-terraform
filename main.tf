d5392bca059c7c4370f2b305ade5ee499e0944e9
Joshua Crunden <joshuacrunden@gmail.com>
2022-01-12 14:22:06 +0000

    Add api custom domain resources

--- main.tf
+++ main.tf
@@ -234,6 +234,63 @@ resource "aws_route53_record" "www_ip6" {
     }
 }
 
+resource "aws_acm_certificate" "cert_local_region" {
+    domain_name = var.domain_name
+    subject_alternative_names = ["*.${var.domain_name}"]
+    validation_method = "DNS"
+}
+
+resource "aws_route53_record" "certification_validation_local_region" {
+    for_each = {
+        for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
+          name   = dvo.resource_record_name
+          record = dvo.resource_record_value
+          type   = dvo.resource_record_type
+        }
+    }
+
+    allow_overwrite = true
+    name            = each.value.name
+    records         = [each.value.record]
+    ttl             = 60
+    type            = each.value.type
+    zone_id = data.aws_route53_zone.philomusica_hosted_zone.zone_id
+}
+
+resource "aws_acm_certificate_validation" "cert_validation_local_region" {
+    certificate_arn = aws_acm_certificate.cert_local_region.arn
+    validation_record_fqdns = [for record in aws_route53_record.certification_validation_local_region : record.fqdn]
+}
+
+resource "aws_route53_record" "api_custom_domain" {
+    name = format("api.%s", var.domain_name)
+    zone_id = data.aws_route53_zone.philomusica_hosted_zone.zone_id
+    type = "A"
+	alias {
+      evaluate_target_health = true
+      name                   = aws_api_gateway_domain_name.api_custom_domain.regional_domain_name
+      zone_id                = aws_api_gateway_domain_name.api_custom_domain.regional_zone_id
+   }
+}
+
+resource "aws_api_gateway_base_path_mapping" "api_custom_domain" {
+  api_id      = aws_api_gateway_rest_api.contact.id
+  base_path   = "contact"
+  domain_name = format("api.%s", var.domain_name)
+  stage_name  = "philomusica"
+}
+
+resource "aws_api_gateway_domain_name" "api_custom_domain" {
+  domain_name              = format("api.%s", var.domain_name)
+  regional_certificate_arn = aws_acm_certificate_validation.cert_validation_local_region.certificate_arn
+
+  endpoint_configuration {
+	types = [
+      "REGIONAL",
+    ]
+  }
+}
+
 resource "aws_iam_role" "lambda_edge_iam_role" {
   assume_role_policy = <<EOF
 {
